0dd1b2c0ec9d2ac086f8454aaa4819cd525e7189
TAN-cookie <mintrayvr@gmail.com>
2022-06-27 01:12:58 +0900

    slide

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from '../../models/movie';
 
 @Component({
     selector: 'slider',
     templateUrl: './slider.component.html',
     styleUrls: ['./slider.component.scss'],
+    animations: [trigger('slideFade', [state('void', style({ opacity: 0 })), transition('void <=> *', [animate('1s')])])],
 })
 export class SliderComponent implements OnInit {
-    constructor() {}
+    @Input() items: Movie[] = [];
 
-    ngOnInit(): void {}
+    currentSlideIndex: number = 0;
+
+    ngOnInit(): void {
+        setTimeout(() => {
+            this.currentSlideIndex = ++this.currentSlideIndex % this.items.length;
+        }, 2000);
+    }
 }
